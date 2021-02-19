15e94a40d144d8e831e46645dde2ac892418e048
Eyo Elijah <eyoelijah@gmail.com>
2021-02-19 17:13:00 +0100

    initial commit

---
+++ codegenerator.php
@@ -0,0 +1,19 @@
+<?php
+echo '<h1>Recharge Card Generator</h1>';
+
+    function randomCardGenerator($digits) {
+    $pins = '';
+
+    for($i = 0; $i < $digits; $i++) {
+        $pins .= mt_rand(0, 9);
+    }
+
+    return $pins;
+    }
+    for($i = 0; $i < 200; $i++){
+    echo randomCardGenerator(12) .'<br>';
+        
+    }
+
+?>
+
